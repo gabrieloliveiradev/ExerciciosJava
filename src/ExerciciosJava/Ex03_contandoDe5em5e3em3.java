fab09c602261f4646e3204abf53af408b4a37c4f
Gabriel Rodrigues de Oliveira <gabriel11.r@hotmail.com>
2020-05-15 16:42:00 -0300

    atualizar exercício 3

--- src/ExerciciosJava/Ex03_contandoDe5em5e3em3.java
+++ src/ExerciciosJava/Ex03_contandoDe5em5e3em3.java
@@ -2,6 +2,12 @@ package ExerciciosJava;
 
 public class Ex03_contandoDe5em5e3em3 {
 	public static void main(String[] args) {
-		
+		for (int i = 0; i <457; i++) {
+			if(i < 300 || i > 400) {
+				i = i + 4;
+			} else {
+				i = i + 2;
+			}
+		}
 	}
 }

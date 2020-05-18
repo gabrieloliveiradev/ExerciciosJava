ee9ed9ef751638c4e00f393476b1cb706baa3cc0
Gabriel Rodrigues de Oliveira <gabriel11.r@hotmail.com>
2020-05-18 08:50:45 -0300

    arrumar erro no exercicio 3

--- src/ExerciciosJava/Ex03_contandoDe5em5e3em3.java
+++ src/ExerciciosJava/Ex03_contandoDe5em5e3em3.java
@@ -6,7 +6,7 @@ package ExerciciosJava;
 
 public class Ex03_contandoDe5em5e3em3 {
 	public static void main(String[] args) {
-		for (int i = 0; i <457; i++) {
+		for (int i = 233; i <457; i++) {
 			if(i < 300 || i > 400) {
 				i = i + 4;
 			} else {
